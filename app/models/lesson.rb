0859b1e5bb7997aa236d8a4b33a041ed2606bb24
ippei.ohshiro <castle.bbstyle@gmail.com>
2019-08-23 16:06:40 +0000

    fix

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,7 +5,7 @@ class Lesson < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :subtitle, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 1000 }
-  validates :comment, presence: true, length: { maximum: 255 }
+  validates :comment, length: { maximum: 255 }
   
   has_many :attendances
   has_many :att_users, through: :attendances, source: :user, dependent: :destroy
